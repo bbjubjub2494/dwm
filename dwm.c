ece514b79bbbae8d70af41f5cfc1a01eff765a7a
Louis Bettens <louis@bettens.info>
2021-12-03 19:16:02 +0100

    tilegaps for the columns layout
    This is a port of the tilegaps patch to apply on top of the columns patch.
    
    Co-Authored-By: astier <aleksandrs.stier@uni-bielefeld.de>

--- dwm.c
+++ dwm.c
@@ -1674,26 +1674,29 @@ tagmon(const Arg *arg)
 void
 col(Monitor *m)
 {
-	unsigned int i, n, h, w, x, y, mw;
+	unsigned int i, n, h, w, x, y, mw, ns;
 	Client *c;
 
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if (n == 0)
 		return;
 
-	if (n > m->nmaster)
+	if (n > m->nmaster) {
 		mw = m->nmaster ? m->ww * m->mfact : 0;
-	else
+		ns = m->nmaster > 0 ? 2 : 1;
+	} else {
 		mw = m->ww;
-	for (i = x = y = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		ns = 1;
+	}
+	for (i = 0, x = y = gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			w = (mw - x) / (MIN(n, m->nmaster) - i);
-			resize(c, x + m->wx, m->wy, w - (2 * c->bw), m->wh - (2 * c->bw), 0);
-			x += WIDTH(c);
+			w = (mw - x) / (MIN(n, m->nmaster) - i) - gappx;
+			resize(c, x + m->wx, m->wy + gappx, w - (2 * c->bw), m->wh - (2 * c->bw) - gappx*(5-ns)/2, 0);
+			x += WIDTH(c) + gappx;
 		} else {
-			h = (m->wh - y) / (n - i);
-			resize(c, x + m->wx, m->wy + y, m->ww - x - (2 * c->bw), h - (2 * c->bw), 0);
-			y += HEIGHT(c);
+			h = (m->wh - y) / (n - i) - gappx;
+			resize(c, m->wx + mw, m->wy + y, m->ww - mw - (2*c->bw) - gappx*(5-ns)/2, h - (2*c->bw), 0);
+			y += HEIGHT(c) + gappx;
 		}
 }
 
